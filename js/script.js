3327ab2117f7c8c2168a396bc0ffb0f02462883c
adham <adhambinahmed@gmail.com>
2026-02-18 10:47:14 +0200

    Upload test-priv-kira website

---
+++ js/script.js
@@ -0,0 +1,71 @@
+function scrollToContact() {
+  document.getElementById("contact").scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+// Fade In عند تحميل الصفحة
+window.addEventListener("load", () => {
+  document.body.classList.add("loaded");
+});
+
+// Fade Out عند الانتقال
+document.querySelectorAll("a").forEach(link => {
+  link.addEventListener("click", function (e) {
+    if (this.href.includes("#")) return;
+
+    e.preventDefault();
+    const target = this.href;
+
+    document.body.classList.remove("loaded");
+    document.body.classList.add("fade-out");
+
+    setTimeout(() => {
+      window.location.href = target;
+    }, 400);
+  });
+});
+
+const toggleBtn = document.getElementById("themeToggle");
+
+// تحميل الوضع المحفوظ
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+  toggleBtn.textContent = "☀️";
+}
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+    toggleBtn.textContent = "☀️";
+  } else {
+    localStorage.setItem("theme", "light");
+    toggleBtn.textContent = "🌙";
+  }
+});
+
+
+// Accordion
+const accBtns = document.querySelectorAll(".accordion-btn");
+
+accBtns.forEach(btn => {
+  btn.addEventListener("click", function () {
+
+    const content = this.nextElementSibling;
+
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+
+      // يقفل أي قائمة مفتوحة
+      document.querySelectorAll(".accordion-content").forEach(item => {
+        item.style.maxHeight = null;
+      });
+
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+
+  });
+});
