0d1a46ef585aad02265eeb906aeeb00061b405a4
AdhamBinAhmed <adhambinahmad@gmail.com>
2026-02-21 18:20:56 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -311,3 +311,17 @@ document.querySelectorAll("a").forEach(link => {
     }
   });
 });
+
+// Loader
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+
+  setTimeout(() => {
+    loader.style.opacity = "0";
+
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 500);
+
+  }, 700); // وقت التحميل (تقدر تقلله)
+});
